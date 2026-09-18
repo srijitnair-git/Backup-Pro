820afa8db1ea638ce73cff94c8cb338cf95566f1
Srijit Nair <srijitnair.in+github@gmail.com>
2026-09-19 02:08:36 +0530

    Implement required onUpdate in Widget Providers

--- android/app/src/main/kotlin/com/matrix/backuppro/SmallWidgetProvider.kt
+++ android/app/src/main/kotlin/com/matrix/backuppro/SmallWidgetProvider.kt
@@ -1,5 +1,25 @@
 package com.matrix.backuppro
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetLaunchIntent
 import es.antonborri.home_widget.HomeWidgetProvider
 
-class SmallWidgetProvider : HomeWidgetProvider()
+class SmallWidgetProvider : HomeWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+        widgetData: SharedPreferences
+    ) {
+        appWidgetIds.forEach { widgetId ->
+            val views = RemoteViews(context.packageName, R.layout.small_widget).apply {
+                val pendingIntent = HomeWidgetLaunchIntent.getActivity(context, MainActivity::class.java)
+                setOnClickPendingIntent(R.id.widget_root, pendingIntent)
+            }
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
+    }
+}
